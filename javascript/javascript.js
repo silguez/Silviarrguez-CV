39d4a961a67afbeeb764ac22e5cf1c66b552f2d4
silguez <slvrr295@gmail.com>
2021-09-20 13:58:02 +0200

    nueva experiencia

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -128,3 +128,14 @@ function categorias() {
 }
 
 
+let animado = document.querySelectorAll(".animado");
+function mostrarScroll(){
+  let scrollTop = document.documentElement.scrollTop;
+  for(var i=0; i < animado.length; i++){
+    let alturaAnimado = animado[i].offsetTop;
+    if(alturaAnimado - 300 < scrollTop){
+      animado[i].style.opacity = 1;
+    }
+  }
+}
+window.addEventListener('scroll', mostrarScroll);
